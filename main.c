ca4ffe6ba9c36ae44bc069ba5d7af7d44c399e08
Pavlos <pavlos_tsaou@hotmail.gr>
2015-11-16 13:38:17 +0200

    Added comments

--- main.c
+++ main.c
@@ -7,12 +7,12 @@ void find( void );
 
 int main(int argc, char const *argv[]){
 
-	int quit = 1; 
-	char answer;
+	int quit = 1; 																				// Checks when to terminate execution
+	char answer;																				// Action answered by user
 
 	while(quit != 0){
-		print_menu();
-		answer = getchar();
+		print_menu();																			// Prints actions menu
+		answer = getchar();																		// Gets the answer
 		switch(answer){
 			case '1':
 				add_new();
@@ -35,43 +35,45 @@ void print_menu(){
 	return;
 }
 
+// Adds a new service
 void add_new(){
 	FILE *fp;
 	char service[20], username[20], password[20];
 
-	printf("Give service.\n");
-	scanf("%s", service);
-	getchar();
+	printf("Give service.\n");																	//
+	scanf("%s", service);																		//
+	getchar();																					//
+																								//
+	printf("Give username.\n");																	//
+	scanf("%s", username);																		// Reads the information to add the new service
+	getchar();																					//
+																								//
+	printf("Give password.\n");																	//
+	scanf("%s", password);																		//
+	getchar();																					//
 
-	printf("Give username.\n");
-	scanf("%s", username);
-	getchar();
-
-	printf("Give password.\n");
-	scanf("%s", password);
-	getchar();
-
-	fp = fopen("storage.txt", "a+");
-	fprintf(fp, "%s %s %s\n", service, username, password );
-	fclose(fp);
+	fp = fopen("storage.txt", "a+");															//
+	fprintf(fp, "%s %s %s\n", service, username, password );									// Stores the new service in database
+	fclose(fp);																					//
 }
 
+// Finds an existing service
 void find(){
 	FILE *fp;
 	char service[20], service_to_find[20], username[20], password[20];
 
-	printf("Give service\n");
-	scanf("%s", service_to_find);
-	getchar();
+	printf("Give service\n");																	//
+	scanf("%s", service_to_find);																// Reads from user the service to search for
+	getchar();																					//
 
 	fp = fopen("storage.txt", "r");
 
-	while(fscanf(fp, "%s %s %s", service, username, password) != EOF){
-		if(strcmp(service, service_to_find) == 0){
-			printf("Service: %s\nUsername: %s\nPassword: %s\n", service, username, password);
-			return;
-		}
+	while(fscanf(fp, "%s %s %s", service, username, password) != EOF){							//
+		if(strcmp(service, service_to_find) == 0){												//
+			printf("Service: %s\nUsername: %s\nPassword: %s\n", service, username, password);	// Searches the entire database untill it finds the service or the End Of File
+			return;																				//
+		}																						//
 	}
-	printf("Service not found!\n");
+	printf("Service not found!\n");																// Alerts user that this service does not exist
 	return;
 }
